2da6e63a2e9938e1017d50b7a5a8c1e046822935
marcosamdc <antoniomarcos_amdc@hotmail.com>
2025-04-21 21:39:56 +0200

    Web Components (Deel 2)

---
+++ src/views/Knowledge.vue
@@ -0,0 +1,11 @@
+<!-- src/views/Knowledge.vue -->
+<template>
+    <v-container>
+      <h1>Knowledge base</h1>
+      <p>Hier komt een lijst met kennisbank</p>
+    </v-container>
+  </template>
+  <script>
+  export default {}
+  </script>
+  
